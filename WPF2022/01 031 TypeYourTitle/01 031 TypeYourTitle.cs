67ad4fab1376314459646a036e18be013ef88486
Солодилов Дмитрий Эдуардович <dsolod00@mail.ru>
2022-03-28 12:30:02 +0500

    add new project

--- WPF2022/01 031 TypeYourTitle/01 031 TypeYourTitle.cs
+++ WPF2022/01 031 TypeYourTitle/01 031 TypeYourTitle.cs
@@ -11,14 +11,14 @@ namespace Petzold.TypeYourTitle
     {         [STAThread]         
         public static void Main()         
         {             
-            Application app = new Application();             
+            Application app = new Application();//создает объект app класса Appliation           
             app.Run(new TypeYourTitle());         
         }         protected override void OnTextInput (TextCompositionEventArgs args)         
         {             
             base.OnTextInput(args);             
             if (args.Text == "\b" && Title.Length > 0)//если уже ввел текст, тогда применим Backspace                 
                 Title = Title.Substring(0, Title .Length - 1);//нажатие Backspace приводит к удалению символа из строки           
-            else if (args.Text.Length > 0 && !Char .IsControl(args.Text[0]))                 
+            else if (args.Text.Length > 0 && !Char .IsControl(args.Text[0]))// присоединение введеного к содержимому                 
                 Title += args.Text;         
         }     
     } 
---
+++ WPF2022/03 066 FormatTheText/03 066 FormatTheText/03 066 FormatTheText.cs
@@ -0,0 +1,38 @@
+﻿
+//---------------------------------------------- 
+//FormatTheText.cs (c) 2006 by Charles Petzold 
+//---------------------------------------------- 
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Documents;
+
+namespace Petzold.FormatTheText
+{
+    class FormatTheText : Window// создаем класс FormatTheText типа Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();// создаем объект app класса Application
+            app.Run(new FormatTheText());
+        }
+        public FormatTheText()// метод класса FormatTheText
+        {
+            Title = "Format the Text";
+            TextBlock txt = new TextBlock();
+            txt.FontSize = 32;// размер выводимого текста
+            txt.Inlines.Add("This is some ");
+            txt.Inlines.Add(new Italic(new Run("italic")));
+            txt.Inlines.Add(" text, and this is some ");
+            txt.Inlines.Add(new Bold(new Run("bold")));
+            txt.Inlines.Add(" text, and let's cap it off with some ");
+            txt.Inlines.Add(new Bold(new Italic(new Run("bold italic"))));
+            txt.Inlines.Add(" text.");
+            txt.TextWrapping = TextWrapping.Wrap;
+            Content = txt;
+        }
+    }
+}
