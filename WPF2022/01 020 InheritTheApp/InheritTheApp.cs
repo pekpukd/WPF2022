4ca62540501252b850258667649481b4871edf48
AnnaKandrina <90605102+AnnaKandrina@users.noreply.github.com>
2022-04-04 10:52:15 +0500

    comments added

--- WPF2022/01 020 InheritTheApp/InheritTheApp.cs
+++ WPF2022/01 020 InheritTheApp/InheritTheApp.cs
@@ -7,26 +7,27 @@ using System.Windows;
 using System.Windows.Input;
 namespace Petzold.InheritTheApp
 {
-    class InheritTheApp : Application
+    class InheritTheApp : Application //класс, производный от Application
     {
-        [STAThread]
+        [STAThread] //используется однопоточная модель
         public static void Main()
         {
-            InheritTheApp app = new InheritTheApp();
-            app.Run();
+            InheritTheApp app = new InheritTheApp(); //создаем объект типа InheritTheApp
+            app.Run(); //вызов метода Run, котрый запускает цикл сообщений
         }
-        protected override void OnStartup(StartupEventArgs args)
+        protected override void OnStartup(StartupEventArgs args) //переопределение метода OnStartu
         {
-            base.OnStartup(args);
-            Window win = new Window();
-            win.Title = "Inherit the App";
-            win.Show();
+            base.OnStartup(args); //вызов метода базового класса
+            Window win = new Window(); //создание объекта типа Window
+            win.Title = "Inherit the App"; //определение текста заголовка окна
+            win.Show(); //отображение окна на экране
         }
-        protected override void OnSessionEnding(SessionEndingCancelEventArgs args)
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs args) //переопределение метода OnSessionEnding
         {
-            base.OnSessionEnding(args);
-            MessageBoxResult result = MessageBox.Show("Do you want to  save your data?", MainWindow.Title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.Yes);
-            args.Cancel = (result == MessageBoxResult.Cancel);
+            base.OnSessionEnding(args); //вызов метода базового класса
+            MessageBoxResult result = MessageBox.Show("Do you want to  save your data?", MainWindow.Title, 
+            MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.Yes); //вывод окна сообщения с кнопками Yes, No и Cancel
+            args.Cancel = (result == MessageBoxResult.Cancel); //установление флага Cancel объекта SessionEndingCancelEventArgs
         }
     }
 }
