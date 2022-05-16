c32c25f38b47540df22e666ea389776ee60b7200
Васильев Андрей Геннадиевич <vasilevandreig@gmail.com>
2022-05-16 14:16:25 +0500

    RenderTheAnimation

---
+++ WPF2022/30 895 RenderTheAnimation/RenderTheAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+namespace Petzold.RenderTheAnimation 
+{ 
+    class RenderTheAnimation : Window 
+    {
+        [STAThread] // управление программой осуществляется одним главным потоком
+
+        public static void Main() // точка входа выполняемой программы
+        { 
+            Application app = new Application(); // создание нового приложения
+            app.Run(new RenderTheAnimation()); // запускает приложение 
+        } 
+        
+        public RenderTheAnimation() { 
+            Title = "Render the Animation"; // задаёт заголовок окна
+            Content = new AnimatedCircle(); // экземпляр класса AnimatedCircle присвоивается содержимому окна 
+        }
+    }
+}
