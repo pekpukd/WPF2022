d61268edcd2a431b40b3e302a6491c4df143b6c7
Хакимова Карина Маратовна <gmsnhg@yandex.ru>
2023-05-20 14:18:11 +0500

    карина молодец

--- WPF2022/00 014 SayHello/SayHello.cs
+++ WPF2022/00 014 SayHello/SayHello.cs
@@ -1,19 +1,5 @@
-﻿/*
- Program dependencies:   <-- Need to include to blank c# project
-    System
-    PresentationCore
-    PresentationCoreFramework
-    System.Windows 
-    WindowsBase 
-    System.Xaml
-*/
-
-// -----------------------------------------
-// SayHello.cs (c) 2006 by Charles Petzold
-// -----------------------------------------
-
-using System;
-using System.Windows;
+﻿using System;
+using System.Windows; // пространство имен, вкл в себя все основные классы wpf
 
 namespace Petzold.SayHello
 {
@@ -22,16 +8,14 @@ namespace Petzold.SayHello
         [STAThread] // Program will use single-threaded model to run
         public static void Main()
         {
-            Window win = new Window(); // Initializing the new object "window" 
-            win.Title = "Say Hello"; // Make title for the window
-            win.Show(); // Display that white box to user on desktop
-
+            Window win = new Window(); // Инициализация нового обхекта Окно
+            win.Title = "Say Hello"; // заголовок для окна
+            win.Show();
             Application app = new Application();
-            app.Run(); // Run the main event cycle that will keep the window open and track all user actions
+            app.Run(); //запускает цикл сообщений
 
             /*
-              // Also the previous code can be simplified into:
-              new Application().Run(new Window {Title = "Say Hello"});
+             
             */
         }
     }
